25ee08f6a908fb4db0ef0ba8980db03524c58718
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-30 21:32:01 +0100

    uv version 🎉

--- docs/release.sh
+++ docs/release.sh
@@ -3,8 +3,8 @@
 
 [[ $(basename "$PWD") == docs ]] && cd ..
 
-OLD=$(uv run --script docs/release.py old)
-NEW=$(uv run --script docs/release.py "$1")
+OLD=$(uv version --short)
+NEW=$(uv version --short --bump "$1")
 DATE=$(date +%Y-%m-%d)
 
 sed -ri "/__version__ /s/[0-9.]+/$NEW/" nmdmail/__init__.py
@@ -12,7 +12,7 @@ sed -i "/^## \[Unreleased\]/a \\\n## [v$NEW] - $DATE" CHANGELOG.md
 sed -i "/^\[Unreleased\]/s/$OLD/$NEW/" CHANGELOG.md
 sed -i "/^\[Unreleased\]/a [v$NEW]: https://github.com/nim65s/nmdmail/compare/v$OLD...v$NEW" CHANGELOG.md
 
-git add nmdmail/__init__.py pyproject.toml CHANGELOG.md
+git add nmdmail/__init__.py pyproject.toml CHANGELOG.md uv.lock
 git commit -m "Release v$NEW"
 git tag -s "v$NEW" -m "Release v$NEW"
 git push
